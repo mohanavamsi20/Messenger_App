94940a0738029f8aaa061406bbbe95c9e43fcbed
Mohana Vamsi <52503554+mohanavamsi20@users.noreply.github.com>
2021-06-09 13:24:45 +0530

    Initial Commit

---
+++ frontend/src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = (props) =>{
+    return(
+        <div>
+            <h1 align="center">Welcome To Messenger App</h1>
+        </div>
+    )
+}
+
+export default Home;
